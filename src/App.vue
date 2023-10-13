985430aba46a312a1350a8f668ef0af4715d76e7
joaovitoras <joaovitor_as08802@hotmail.com>
2023-10-13 14:15:29 -0300

    Add initial styles

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <AppHome />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import AppHome from './components/AppHome.vue'
 
 export default defineComponent({
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    AppHome
+  },
+  name: 'App'
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/styles/global.css';
 </style>
---
+++ src/components/AppHome.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="main-section">
+    <h1>JSON Tree Viewer</h1>
+
+    <p>Simple JSON Viewer that runs completely on-client. No data exchange</p>
+
+    <button>Load JSON</button>
+
+    <p class="invalid" v-if="invalid">
+      Invalid file. Please load a valid JSON file.
+    </p>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AppHome',
+  data () {
+    return {
+      invalid: false
+    }
+  }
+})
+</script>
